884d43d4f951bb4b491f684cec8af0dee07e41f9
Ahmad-AL-Ahmady <ahmad.hussain.alahmady@gmail.com>
2024-09-28 23:20:15 +0300

    "Refactor error handling: extract global error handler to separate controller, add error handling for CastError, ValidationError, and duplicate errors"

--- Blogging-Platform-API/app.js
+++ Blogging-Platform-API/app.js
@@ -3,15 +3,17 @@ const morgan = require('morgan');
 
 const postRouter = require('./routes/postRoutes');
 const AppError = require('./utils/AppError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log('Middleware Working 🚨');
+  console.log('Middleware is Working 🚨');
   next();
 });
-app.use(morgan('dev'));
+
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
 app.use('/api/v1/posts', postRouter);
 
@@ -19,12 +21,6 @@ app.all('*', (req, res, next) => {
   next(new AppError('Route not found', 404));
 });
 
-app.use((err, req, res, next) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-    data: null,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
--- Blogging-Platform-API/controllers/errorController.js
+++ Blogging-Platform-API/controllers/errorController.js
@@ -1,13 +1,44 @@
-// const AppError = require('../utils/appError');
+const AppError = require('../utils/AppError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateErrorDB = (err) => {
+  const value = err.keyValue.title;
+  const message = `Duplicate value: ${value}. Please change this value and try again.`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = errors.join('. ');
+  return new AppError(message, 400);
+};
 
 const sendErrorDev = (err, res) => {
-  console.error('ERROR:', err);
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
     error: err,
     stack: err.stack,
   });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error('ERROR ❗⛔:', err);
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong, please try again later.',
+    });
+  }
 };
 
 module.exports = (err, req, res, next) => {
@@ -17,6 +48,12 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    console.log('production');
+    let error = Object.assign(err);
+
+    if (err.name === 'CastError') error = handleCastErrorDB(err);
+    if (err.name === 'ValidationError') error = handleValidationErrorDB(err);
+    if (err.code === 11000) error = handleDuplicateErrorDB(err);
+
+    sendErrorProd(error, res);
   }
 };
